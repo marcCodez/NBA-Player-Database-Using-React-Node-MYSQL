60cdfe278fe6c82899d71400f0338b645a40a458
marcCodez <marc_pulumbarit@hotmail.com>
2020-12-07 20:40:07 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>NBA Player Database</h1>
+    <div className="playerForm">
+    <label>Name: </label>
+    <input type="text" name="playerName"/>
+    <label>Age: </label><input type="text" name="playerAge"/>
+    <label>Height: </label><input type="text" name="playerHeight"/>
+    <label>Position:</label> <input type="text" name="playerPosition"/>
+    <button>Submit</button>
+    </div>
     </div>
   );
 }
