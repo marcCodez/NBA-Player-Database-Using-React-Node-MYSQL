5160a2f1318d1c2349f86d6251ead857383168e8
marcCodez <marc_pulumbarit@hotmail.com>
2020-12-08 21:09:13 +1100

    axios get players and post players to mysql database working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
 function App() {
 
@@ -8,24 +10,70 @@ function App() {
   const [age, setAge] = useState('')
   const [height, setHeight] = useState('')
   const [position, setPosition] = useState('')
+  const [playerList, setPlayerList] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/nbadb/get').then((response) => {
+    setPlayerList(response.data)
+    })
+  })
+
+  const submitPlayer = () => {
+    axios.post("http://localhost:3001/nbadb/insert", {
+      firstName: firstName, 
+      lastName: lastName, 
+      age: age, 
+      height: height, 
+      position: position
+    }).then(() => {
+      alert("player submitted sucessfully")
+    })
+  }
 
   return (
     <div className="App">
     <h1>NBA Player Database</h1>
 
-    <div className="playerForm">
+    <div className="form">
     <label>First Name: </label>
     <input type="text" name="playerName" onChange={(e) => setFirstName(e.target.value)}/>
     <label>Last Name: </label>
-    <input type="text" name="playerName" onChange={(e) => setLastName(e.target.value)}/>
+    <input type="text" name="lastName" onChange={(e) => setLastName(e.target.value)}/>
     <label>Age: </label>
-    <input type="text" name="playerAge"/>
+    <input type="number" name="playerAge" onChange={(e) => setAge(e.target.value)}/>
     <label>Height: </label>
-    <input type="text" name="playerHeight"/>
+    <input type="number" name="playerHeight" onChange={(e) => setHeight(e.target.value)}/>
     <label>Position:</label> 
-    <input type="text" name="playerPosition"/>
+    <input type="text" name="playerPosition" onChange={(e) => setPosition(e.target.value)}/>
 
-    <button>Submit</button>
+    <button onClick={submitPlayer}>Submit</button>
+  
+  <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Age</th>
+        <th>Height</th>
+        <th>Position</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        {playerList.map((player) => {
+          return (
+            <>
+          <td>{player.firstName}</td>
+          <td>{player.lastName}</td>
+          <td>{player.age}</td>
+          <td>{player.height}</td>
+          <td>{player.location}</td>
+          </>
+          );
+        })}
+      </tr>
+    </tbody>
+    </Table>
     </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const mysql = require('mysql');
 
@@ -9,9 +11,26 @@ const db = mysql.createPool({
    database: 'NBAPlayerDatabase'
 });
 
-app.get('/', (req, res) => {
-    
-   
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/nbadb/get', (req, res) => {
+
+    const sql = "SELECT * FROM nba_players"
+   db.query(sql, (err, result) => {
+    res.send(result)
+   });
+});
+
+
+app.post('/nbadb/insert', (req, res) => {
+    const { firstName, lastName, age, height, position } = req.body
+ 
+    const sql = "INSERT INTO nba_players (first_name, last_name, age, height, position) VALUES (?,?,?,?,?)"
+   db.query(sql, [firstName, lastName, age, height, position], (err, result) => {
+    console.log(result)
+   });
 });
 
 app.listen(3001, () => {
