868b40200d405894521db3956256ac3c7c4c82e7
marcCodez <marc_pulumbarit@hotmail.com>
2020-12-08 18:18:57 +1100

    server running with mysql connection

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+   host: 'localhost',
+   user: 'root',
+   password: 'password',
+   database: 'NBAPlayerDatabase'
+});
+
+app.get('/', (req, res) => {
+  
+   
+});
+
+app.listen(3001, () => {
+    console.log('Server is up on port 3001');
+});
